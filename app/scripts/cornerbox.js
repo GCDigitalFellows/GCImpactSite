341f51c449cad2570e308c9cbee10c6412425594
Keith Miyake <kmiyake@gc.cuny.edu>
2015-02-19 18:34:06 -0500

    add map overlay and tweak the displaying of the menu

--- app/scripts/cornerbox.js
+++ app/scripts/cornerbox.js
@@ -1,19 +1,37 @@
+'use strict';
 /**
  * main.js
  * http://www.codrops.com
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2014, Codrops
  * http://www.codrops.com
  */
 (function() {
 
-	var bodyEl = document.body,
-		content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
+	/**
+	 * based on from https://github.com/inuyaksa/jquery.nicescroll/blob/master/jquery.nicescroll.js
+	 */
+	function hasParent( e, p ) {
+		if (!e) {
+			return false;
+		}
+		var el = e.target||e.srcElement||e||false;
+		while (el && el !== p) {
+			el = el.parentNode||false;
+		}
+		return (el!==false);
+	}
+
+	var openbtn = document.getElementById( 'open-button' ),
 		closebtn = document.getElementById( 'close-button' ),
+		openMapbtn = document.getElementById( 'map-button' ),
+		closeMapbtn = document.getElementById( 'close-map-button' ),
+		menu = document.querySelector('.menu-wrap'),
+		map = document.querySelector('.map-wrap'),
+		isMapOpen = false,
 		isOpen = false;
 
 	function init() {
@@ -21,28 +39,41 @@
 	}
 
 	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
+		openbtn.addEventListener( 'click', function() {
+			toggleMenu();
+			openbtn.blur();
+		} );
+		openMapbtn.addEventListener( 'click', function() {
+			toggleMap();
+			event.preventDefault();
+		});
 		if( closebtn ) {
 			closebtn.addEventListener( 'click', toggleMenu );
 		}
+		if( closeMapbtn ) {
+			closeMapbtn.addEventListener( 'click', toggleMap );
+		}
 
-		// close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
+		// close the menu element if the target is not the menu element or one of its descendants..
+		document.addEventListener( 'mousedown', function(ev) {
 			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
+			console.log(target);
+			if( isOpen && target !== openbtn && !hasParent( target, menu ) ) {
 				toggleMenu();
+			} else if ( isMapOpen && target !== openMapbtn && !hasParent( target, map ) ) {
+				toggleMap();
 			}
 		} );
 	}
 
 	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
-		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
+		document.body.classList.toggle('show-menu');
 		isOpen = !isOpen;
+	}
+	function toggleMap() {
+		console.log('pressed');
+		document.body.classList.toggle('show-map');
+		isMapOpen = !isMapOpen;
 	}
 
 	init();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,26 @@
 'use strict';
 
 $(document).ready(function() {
-    var currSection = 1, currSlide = 0, 
-        mySectionSelector = '.section', 
-        mySlideSelector = '.slide',
-        sectionCount = $(mySlideSelector).length - 1;
+    // var currSection = 1, currSlide = 0,
+        // mySectionSelector = '.section',
+        // mySlideSelector = '.slide',
+        // sectionCount = $(mySlideSelector).length - 1;
     $('#fullpage').fullpage({
         verticalCentered: true,
         resize : false,
-		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE','#fff'],
-        anchors:['section1', 'section2', 'section3', 'section4'],
+		//sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE','#fff'],
+        anchors:['section1', 'section2', 'section3', 'section4','section5','section6','section7','section8','section9','section10','section11'],
         scrollingSpeed: 700,
         easing: 'easeInQuart',
         menu: '#headerMenu',
         navigation: true,
-        navigationPosition: 'left',
+        navigationPosition: 'right',
         navigationTooltips: ['Intro', 'Research', 'Teaching', 'People'],
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
         loopBottom: true,
         loopTop: false,
-        loopHorizontal: true,
+        loopHorizontal: false,
         autoScrolling: true,
         scrollOverflow: true,
         css3: true,
@@ -36,75 +36,81 @@ $(document).ready(function() {
         slideSelector: '.slide',
         responsive: 768,
 
-        afterRender: function() {
-            currSection = 1;
-            currSlide = 0;
-        },
+        // afterRender: function() {
+        //     currSection = 1;
+        //     currSlide = 0;
+        // },
 
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
-            currSection = index;
-            currSlide = slideIndex;
-            //console.log('afterloadslide sec: ' + currSection + ' slide: ' + currSlide);
-            var $currSection = $(mySectionSelector).eq(currSection-1);
-            var slideCount =  $currSection.find(mySlideSelector).length;
-            if (index === sectionCount && slideCount - 1 === slideIndex) {
-                $('.bottom-nav #bottom-next').addClass('hidden');
-            } else if (index === 1 && slideIndex === 0) {
-                $('.bottom-nav #bottom-prev').addClass('hidden');
-            }
-        },
+        //afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+            // currSection = index;
+            // currSlide = slideIndex;
+            // //console.log('afterloadslide sec: ' + currSection + ' slide: ' + currSlide);
+            // var $currSection = $(mySectionSelector).eq(currSection-1);
+            // var slideCount =  $currSection.find(mySlideSelector).length;
+            // if (index === sectionCount && slideCount - 1 === slideIndex) {
+            //     $('.bottom-nav #bottom-next').addClass('hidden');
+            // } else if (index === 1 && slideIndex === 0) {
+            //     $('.bottom-nav #bottom-prev').addClass('hidden');
+            // }
+        //},
         afterLoad: function(anchorLink, index){
-            currSection = index;
-            var $currSection = $(mySectionSelector).eq(currSection-1);
-            var slideCount =  $currSection.find(mySlideSelector).length;
-            if (slideCount === 0) {
-                currSlide = 0;
-            } else {
-                currSlide = $currSection.find(mySlideSelector + '.active').index();
+
+            if (anchorLink === 'section2' || index === 2) {
+                /* velocity.js animate svgs */
+                $('#open-map-button').velocity({scale: 0.8,opacity:0.4},{'loop': 2, duration: 1000});
             }
-            //console.log('afterloadsec sec: ' + currSection + ' slide: ' + currSlide + ' slideCount:' + slideCount);
-            if (index === sectionCount && slideCount === 0) {
-                //console.log('disnext');
-                $('.bottom-nav #bottom-next').addClass('hidden');
-                $('.bottom-nav #bottom-top').removeClass('hidden');
-            } else if (index === 1) {
-                console.log('disprev');
-                $('.bottom-nav #bottom-prev').addClass('hidden');
-            }
+
+            // currSection = index;
+            // var $currSection = $(mySectionSelector).eq(currSection-1);
+            // var slideCount =  $currSection.find(mySlideSelector).length;
+            // if (slideCount === 0) {
+            //     currSlide = 0;
+            // } else {
+            //     currSlide = $currSection.find(mySlideSelector + '.active').index();
+            // }
+            // //console.log('afterloadsec sec: ' + currSection + ' slide: ' + currSlide + ' slideCount:' + slideCount);
+            // if (index === sectionCount && slideCount === 0) {
+            //     //console.log('disnext');
+            //     $('.bottom-nav #bottom-next').addClass('hidden');
+            //     $('.bottom-nav #bottom-top').removeClass('hidden');
+            // } else if (index === 1) {
+            //     $('.bottom-nav #bottom-prev').addClass('hidden');
+            // }
         },
-        onLeave: function() {//index, nextIndex, direction) {
-            if ($('.bottom-nav #bottom-prev').hasClass('hidden')) {
-                $('.bottom-nav #bottom-prev').removeClass('hidden');
-            } else if ($('.bottom-nav #bottom-next').hasClass('hidden')) {
-                $('.bottom-nav #bottom-next').removeClass('hidden');
-                $('.bottom-nav #bottom-top').addClass('hidden');
-            }
-        }
+        //onLeave: function() {//index, nextIndex, direction) {
+            // if ($('.bottom-nav #bottom-prev').hasClass('hidden')) {
+            //     $('.bottom-nav #bottom-prev').removeClass('hidden');
+            // } else if ($('.bottom-nav #bottom-next').hasClass('hidden')) {
+            //     $('.bottom-nav #bottom-next').removeClass('hidden');
+            //     $('.bottom-nav #bottom-top').addClass('hidden');
+            // }
+        //}
     });
 
-    $('.bottom-nav button').click(function(){
-        //console.log('currslide: ' + currSlide + ' currsection: ' + currSection);
-        var $currSection, slideCount;
-        if ($(this).attr('id') === 'bottom-top') {
-            $.fn.fullpage.moveTo(1,1);
-        } else if ($(this).attr('id') === 'bottom-prev') {
-            if (currSlide === 0 && currSection > 1) {
-                // get the previous section, see if it has more than 1 slide
-                $currSection = $(mySectionSelector).eq(currSection-2);
-                slideCount =  $currSection.find(mySlideSelector).length;
-                //console.log('prev section: ' + (currSection-1) + ' side count: ' + slideCount);
-                $.fn.fullpage.moveTo(currSection-1,currSlide - 1);
-            } else {
-                $.fn.fullpage.moveSlideLeft();
-            }
-        } else {
-            $currSection = $(mySectionSelector).eq(currSection-1);
-            slideCount =  $currSection.find(mySlideSelector).length;
-            if (currSlide >= 0 && slideCount - 1 > currSlide) {
-                $.fn.fullpage.moveTo(currSection,currSlide + 1);
-            } else {
-                $.fn.fullpage.moveTo(currSection + 1,0);
-            }
-        }
-    });
+    // $('.bottom-nav button').click(function(){
+    //     //console.log('currslide: ' + currSlide + ' currsection: ' + currSection);
+    //     var $currSection, slideCount;
+    //     if ($(this).attr('id') === 'bottom-top') {
+    //         $.fn.fullpage.moveTo(1,1);
+    //     } else if ($(this).attr('id') === 'bottom-prev') {
+    //         if (currSlide === 0 && currSection > 1) {
+    //             // get the previous section, see if it has more than 1 slide
+    //             $currSection = $(mySectionSelector).eq(currSection-2);
+    //             slideCount =  $currSection.find(mySlideSelector).length;
+    //             //console.log('prev section: ' + (currSection-1) + ' side count: ' + slideCount);
+    //             $.fn.fullpage.moveTo(currSection-1,currSlide - 1);
+    //         } else {
+    //             $.fn.fullpage.moveSlideLeft();
+    //         }
+    //     } else {
+    //         $currSection = $(mySectionSelector).eq(currSection-1);
+    //         slideCount =  $currSection.find(mySlideSelector).length;
+    //         if (currSlide >= 0 && slideCount - 1 > currSlide) {
+    //             $.fn.fullpage.moveTo(currSection,currSlide + 1);
+    //         } else {
+    //             $.fn.fullpage.moveTo(currSection + 1,0);
+    //         }
+    //     }
+    // });
+
 });
