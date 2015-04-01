2ea64410d1f42dfe9629bf00866c6af8c6b8bcd7
Keith Miyake <kmiyake@gc.cuny.edu>
2015-04-01 15:06:43 -0400

    materialize map modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -42,34 +42,52 @@ $(document).ready(function() {
 
         },
 
+        onLeave: function() {
+            if (modalTarget && isModalOpen) {
+                $(modalTarget).closeModal();
+                isModalOpen = !isModalOpen;
+            }
+        }
+
     });
 
     /* menu and map handling */
 
-    var isMapOpen = false,
+    var isModalOpen = false,
         isOpen = false,
-        mapId;
+        modalTarget;
+        //mapId;
 
     function toggleMenu() {
         $('body').toggleClass('show-menu');
         isOpen = !isOpen;
     }
-    function toggleMap() {
-        $('body').toggleClass('show-map');
-        isMapOpen = !isMapOpen;
-    }
+    // function toggleMap() {
+    //     $('body').toggleClass('show-map');
+    //     isModalOpen = !isModalOpen;
+    // }
 
     $('#open-button').click(function() {
         toggleMenu();
         $(this).blur();
     });
 
-    $('.map-button').click(function(event) {
-        event.preventDefault();
-        mapId = $(this).data( 'map' );
-        // load the map into the map-wrapper's iframe
-        $('#map-frame').attr('src',mapId);
-        toggleMap();
+    // $('.map-button').click(function(event) {
+    //     event.preventDefault();
+    //     mapId = $(this).data( 'map' );
+    //     // load the map into the map-wrapper's iframe
+    //     $('#map-frame').attr('src',mapId);
+    //     toggleMap();
+    // });
+    $('.modal-trigger').click( function() {
+        var mapSrc = $(this).data('map');
+        modalTarget = $(this).attr('href');
+        var options = {
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        };
+        $('#map-frame').attr('src',mapSrc);
+        $(modalTarget).openModal(options);
+        isModalOpen = !isModalOpen;
     });
 
 
@@ -78,17 +96,19 @@ $(document).ready(function() {
         var $target = $(event.target);
         if( isOpen && !$target.is('#open-button') && $target.closest('.menu-wrap').length === 0) {//!hasParent( target, menu ) ) {
             toggleMenu();
-        } else if ( isMapOpen && ($target.is('#close-map-button') || $target.closest('#map-wrap').length === 0) ) {
-            toggleMap();
+        } else if ( isModalOpen && ($target.is('#close-map-button') || $target.closest('#map-wrap').length === 0) ) {
+            $(modalTarget).closeModal();
+            isModalOpen = !isModalOpen;
         }
     });
 
     $(document).keydown(function( event ) {
         if ( isOpen && event.which === 27) {
             toggleMenu();
-        } else if ( isMapOpen && mapId) {
-            toggleMap();
         }
+        // } else if ( isModalOpen && mapId) {
+        //     toggleMap();
+        // }
     });
 
 });
