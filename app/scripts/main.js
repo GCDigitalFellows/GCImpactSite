c92c34fd61f4e2fafd927a05e0c4bee5c1c12122
Keith Miyake <keith.miyake@gmail.com>
2015-05-27 01:42:34 -0400

    add final slide with link out

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   'use strict';
 
-var isModalOpen = false,
+var isLoaded = false,
+isModalOpen = false,
 isOpen = false,
 modalTarget;
 
@@ -9,7 +10,7 @@ modalTarget;
 $('#fullpage').fullpage({
   animateAnchor: true,
   autoScrolling: true,
-  anchors:['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9', 'section10', 'section11', 'section12'],
+  anchors:['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7', 'section8', 'section9', 'section10', 'section11', 'section12', 'section13'],
   continuousVertical: false,
   css3: true,
   easing: 'easeInOutQuart',//'cubic-bezier(.4, 0, .2, 1)',//'cubic-bezier(0.190, 1.000, 0.220, 1.000)'
@@ -39,7 +40,10 @@ $('#fullpage').fullpage({
 
   afterLoad: function(anchorLink, index){
     // display the site by removing the hiding class
-    $('.loading').removeClass('loading');
+    if (!isLoaded) {
+      $('.loading').removeClass('loading');
+      isLoaded = true;
+    }
 
     var section = Math.ceil(index/3),
         selector = '[data-menusection="' + section + '"]',
@@ -47,11 +51,18 @@ $('#fullpage').fullpage({
     $(selectorNot).removeClass('active');
     $(selector).addClass('active');
 
+    if (index === 13) {
+      $('.card-more').css('opacity', 1);
+    }
+
   },
 
-  onLeave: function() {
+  onLeave: function(index) {
     if (modalTarget && isModalOpen) {
       $('#' + modalTarget).closeModal();
+    }
+    if (index === 13) {
+      $('.card-more').css('opacity', 0);
     }
   }
 
